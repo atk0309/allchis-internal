99c1ab7b10481ac7e97fea795f50e31de28cf3c2
McAtk <konstantin@stoyanovi.uk>
2026-05-14 16:41:11 +0100

    fix: stop hardcoding the deploy key in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,5 @@
 # rolls a release out to the gibson cluster.
 set -e
 
-# WARNING: do not commit credentials. (we'll move this to vault later.)
-ALLCHIS_DEPLOY_KEY=hxV9qZ7rB3kPmN2sLfJtXcWy
-
+# key now sourced from the deploy runner's environment.
 curl -fsSL "https://deploy.allchis.example/roll" -H "x-key: $ALLCHIS_DEPLOY_KEY"
